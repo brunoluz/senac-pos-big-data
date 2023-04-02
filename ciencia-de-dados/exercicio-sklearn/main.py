956cd5e239982453420bec790d6e6705f6ff3e87
Bruno Luz <bruno.al.luz@gmail.com>
2023-04-02 00:04:54 -0300

    dataset prep evolution

--- ciencia-de-dados/exercicio-sklearn/main.py
+++ ciencia-de-dados/exercicio-sklearn/main.py
@@ -10,34 +10,56 @@ from pandas import read_excel
 from pandas.plotting import scatter_matrix
 
 
+def column_to_number(df, original_column_name, numerical_column_name):
+    category = df[original_column_name].astype('category')
+    df[numerical_column_name] = category.cat.codes
+    return dict(enumerate(category.cat.categories))
+
+
 def execute():
-    dataset = read_excel("renda_anual.xlsx")
+    df = read_excel("renda_anual_02.xlsx")
 
-    print(f"dataset shape (rows, columns): {dataset.shape}")
+    tipo_emprego_codes = column_to_number(df, 'tipo_emprego', 'tipo_emprego_cod')
+    estado_civil_codes = column_to_number(df, 'estado_civil', 'estado_civil_cod')
+    emprego_codes = column_to_number(df, 'emprego', 'emprego_cod')
+    relacionamento_codes = column_to_number(df, 'relacionamento', 'relacionamento_cod')
+    raca_codes = column_to_number(df, 'raca', 'raca_cod')
+    sexo_codes = column_to_number(df, 'sexo', 'sexo_cod')
+    pais_de_origem_codes = column_to_number(df, 'pais_de_origem', 'pais_de_origem_cod')
 
-    print("dataset head:")
-    print(dataset.head())
+    print(f"tipo_emprego: {tipo_emprego_codes}")
+    print(f"estado_civil: {estado_civil_codes}")
+    print(f"emprego: {emprego_codes}")
+    print(f"relacionamento: {relacionamento_codes}")
+    print(f"raca: {raca_codes}")
+    print(f"sexo: {sexo_codes}")
+    print(f"pais_de_origem: {pais_de_origem_codes}")
 
-    print("dataset describe:")
-    print(dataset.describe())
+    print(f"df shape (rows, columns): {df.shape}")
 
-    print("dataset info:")
-    print(dataset.info())
+    print("df head:")
+    print(df.head())
+
+    print("df describe:")
+    print(df.describe())
+
+    print("df info:")
+    print(df.info())
 
     # class distribution
-    print("dataset class distribution:")
-    print(dataset.groupby('faixa de renda anual').size())
+    print("df class distribution:")
+    print(df.groupby('faixa_renda_anual').size())
 
     # box and whisker plots
-    dataset.plot(kind='box', subplots=True, layout=(3, 3), sharex=False, sharey=False)
+    df.plot(kind='box', subplots=True, layout=(4, 4), sharex=False, sharey=False)
     pyplot.show()
 
     # histograms
-    dataset.hist()
+    df.hist()
     pyplot.show()
 
     # scatter plot matrix
-    scatter_matrix(dataset)
+    scatter_matrix(df)
     pyplot.show()
 
 
